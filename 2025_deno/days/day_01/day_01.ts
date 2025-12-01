6468874c451cf4e0cfde26af7143cf5ea649560f
Julian <julian.steinacher@students.fhnw.ch>
2025-12-01 13:00:10 +0100

    feat: silver day 01

--- 2025_deno/days/day_01/day_01.ts
+++ 2025_deno/days/day_01/day_01.ts
@@ -1,5 +1,23 @@
 export default function run(input: string){
-    console.log(input)
+    let dial = 50;
+    let result = 0;
 
-    return "Day 01 solution executed.";
+    const lines = input.split("\r\n");
+
+    lines.forEach(line => {
+        const direction = line.charAt(0);
+        const amount = parseInt(line.slice(1));
+
+        const sign = direction === "L" ? -1 : 1;
+
+        dial = mod(dial +(sign* amount), 100);
+        if (dial == 0) result ++;
+
+    })
+
+    return "Final Result: " + result;
+}
+
+function mod(n: number, m: number): number {
+    return ((n % m) + m) % m; // Ensures a positive result
 }
