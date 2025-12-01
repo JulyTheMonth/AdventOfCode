5e52a678d99e45cfd1290d1f0e1c751a2565070e
Julian <julian.steinacher@students.fhnw.ch>
2025-12-01 14:31:00 +0100

    feat: silver day 01 | gold

--- 2025_deno/days/day_01/day_01.ts
+++ 2025_deno/days/day_01/day_01.ts
@@ -1,20 +1,39 @@
+import { diff } from "jsr:@std/internal@^1.0.12/diff";
+
 export default function run(input: string){
     let dial = 50;
     let result = 0;
 
     const lines = input.split("\r\n");
 
+
     lines.forEach(line => {
+
+
         const direction = line.charAt(0);
         const amount = parseInt(line.slice(1));
 
         const sign = direction === "L" ? -1 : 1;
 
-        dial = mod(dial +(sign* amount), 100);
-        if (dial == 0) result ++;
+        const amountToChange = sign * amount;
+
+        const unModDial = dial + amountToChange;
+
+        // Edge case for crossing zero once
+        if (dial >0 && unModDial < 0) result++;
+
+        // Counts how many time it crosses zero.
+        const timesOverHundred = Math.floor(Math.abs(unModDial) / 100);
+        result += timesOverHundred;
+
+        dial = mod(dial +amountToChange, 100);
+
+        // Check if lands exactly on zero without modding
+        if (unModDial == 0) result++;
+
 
     })
-
+    console.log(mod(950,100))
     return "Final Result: " + result;
 }
 
