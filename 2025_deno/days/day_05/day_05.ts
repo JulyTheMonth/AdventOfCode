44d5806e00d3639bd41ab1ea692b505bae5430e3
Julian <julian.steinacher@students.fhnw.ch>
2025-12-06 01:41:58 +0100

    feat: Day 05 gold

--- 2025_deno/days/day_05/day_05.ts
+++ 2025_deno/days/day_05/day_05.ts
@@ -1,3 +1,5 @@
+import $display = Deno.jupyter.$display;
+
 export default function run(input: string) {
   let result = 0;
   let lastResult = -1;
@@ -6,13 +8,29 @@ export default function run(input: string) {
 
   const emptyLineIndex = lines.indexOf("");
 
-  let ranges = lines.slice(0 , emptyLineIndex);
-  let ids = lines.slice(emptyLineIndex + 1);
+  let ranges = lines.slice(0, emptyLineIndex);
 
-  const validIds = ids.filter(id => ranges.find(range => {
-    const [_, minStr, maxStr] = /(\d+)-(\d+)/.exec(range)!;
-    return Number(id) <= Number(maxStr) && Number(id) >= Number(minStr);
-  }));
+  ranges = ranges.map((range) => range.split("-").map(s=>Number(s))).sort((a, b) =>
+    Number(a[0]) - Number(b[0])
+  );
 
-  return "Final Result: " + validIds.length;
+  let mergedRanges: string[][] = [ranges[0]];
+
+  for (let i = 1; i < ranges.length; i++) {
+      const currentRange = ranges[i];
+      const lastRange = mergedRanges[mergedRanges.length-1];
+
+      if (currentRange[0] <= lastRange[1]){
+          lastRange[1] = Math.max(lastRange[1], currentRange[1]);
+      } else {
+        mergedRanges.push(currentRange);
+      }
+
+  }
+
+  mergedRanges.forEach(range =>{
+      result += Number(range[1]) + 1 - Number(range[0]);
+  })
+
+  return "Final Result: " + result;
 }
