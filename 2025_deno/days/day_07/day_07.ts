04bb002bc8b8e665a9993cb85bf812403029c033
Julian <julian.steinacher@students.fhnw.ch>
2025-12-09 11:40:24 +0100

    feat: Day 07 gold

--- 2025_deno/days/day_07/day_07.ts
+++ 2025_deno/days/day_07/day_07.ts
@@ -3,24 +3,30 @@ import $display = Deno.jupyter.$display;
 export default function run(input: string) {
   let result = 0;
 
-  let lines = input.split("\n").map(s=>s.split(""));
+  let lines:number[][] = input.split("\n").map(s=> {
+      return s.split("").map(c=>{
+          if (c=='^') return -1;
+          if (c=='S') return 1;
+          if (c=='.') return 0;
+      })
+  });
 
   for (let i = 1; i < lines.length; i++) {
       const line = lines[i];
       const prevLine = lines[i-1];
       for (let j = 0; j < line.length; j++) {
-          if(line[j] == '^'){
-              if (prevLine[j] =='|' || prevLine[j] == 'S'){
-                  result++;
-                  line[j+1] = '|'; line[j-1] ='|';
+          if(line[j] == -1){
+              if (prevLine[j] >0){
+                  line[j+1]+=prevLine[j]; line[j-1]+=prevLine[j];
               }
           } else{
-              if (prevLine[j] == '|' || prevLine[j] =='S')  line[j] = '|';
+              if (prevLine[j] > 0) line[j] += prevLine[j];
           }
       }
   }
 
-
+  lines.forEach(l => console.log(l.map(s=>s.toString().padStart(3," ")).join(".")))
+    lines.pop().forEach(c=>result+=c);
   // lines.forEach(l=> {
   //     if (l.includes('^')){
   //         l.forEach(c=>{
